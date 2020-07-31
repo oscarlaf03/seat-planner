91bbc0ed71c1f1f0adc8c579d94e80c007877627
oscar ortiz <ortizg.oscar@gmail.com>
2020-07-31 09:26:52 -0300

    routing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { Provider } from 'react-redux'
+import configureStore from '../configureStore'
+const store = configureStore();
+
 import HelloWorld from "./HelloWorld"
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path ="/" render={() => ('Home!') }/>
-          <Route path ="/hello" render={() => <HelloWorld greeting="Friend" />}/>
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path ="/" render={() => ('Home Alone') }/>
+            <Route path ="/hello" render={() => <HelloWorld greeting=" My  Dear Friend" />}/>
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
