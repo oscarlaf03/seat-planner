51db93da1ccaf6ba72673507d676d9156b7ca02d
oscar ortiz <ortizg.oscar@gmail.com>
2020-08-01 13:52:31 -0300

    redux-thunk fetch() configured

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -2,21 +2,43 @@ import React from "react"
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect'
 const GET_SEATS_REQUEST = 'GET_SEATS_REQUEST';
+const GET_SEATS_SUCCESS = 'GET_SEATS_SUCCESS';
 
 function getSeats() {
   console.log(' getSeats() Action!!');
-  return {
+  return dispatch => {
+    dispatch({
     type: GET_SEATS_REQUEST
+    });
+    return fetch('v1/seats.json')
+      .then(response => response.json())
+      .then(json => dispatch(getSeatsSuccess(json)))
+      .catch(error => console.error(error.stack))
   }
 }
 
-import PropTypes from "prop-types"
+export function getSeatsSuccess(json){
+  return {
+    type: GET_SEATS_SUCCESS,
+    json
+  }
+}
+
+// import PropTypes from "prop-types"
 class HelloWorld extends React.Component {
   render () {
+    const { seats } = this.props;
+    const seatsList  = seats.map( (seat) =>{
+    return <li>{seat.name}*---*{seat.guid}</li>
+    });
     return (
       <React.Fragment key={1}>
         Greeting: {this.props.greeting}
+        <br />
         <button className='getSeatsBtn' onClick={ () => this.props.getSeats()}>getSeats</button>
+        <br />
+        <ul> {seatsList} </ul>
+
       </React.Fragment>
     );
   }
--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,18 +1,29 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 
 const initialState = {
-    seats: []
+    seats: [
+        {
+            name:'My first test seat',
+            guid: '123'
+        }
+    ]
 };
 
 function rootReducer(state,action){
     console.log(action.type);
     switch(action.type){
-        default:
-            return state
+        case 'GET_SEATS_SUCCESS':
+            return {seats: action.json.seats}
     }
+    return state;
 }
 
 export default function configureStore(){
-    const store = createStore(rootReducer, initialState);
+    const store = createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(thunk)
+    );
     return store;
 }
