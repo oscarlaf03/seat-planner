d1250f3eb0d6194083745dd34ff7b38e805b97fb
oscar ortiz <ortizg.oscar@gmail.com>
2020-08-01 14:31:00 -0300

    added semantic ui library"

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect'
+import { List } from "semantic-ui-react";
 const GET_SEATS_REQUEST = 'GET_SEATS_REQUEST';
 const GET_SEATS_SUCCESS = 'GET_SEATS_SUCCESS';
 
@@ -29,16 +30,22 @@ class HelloWorld extends React.Component {
   render () {
     const { seats } = this.props;
     const seatsList  = seats.map( (seat) =>{
-    return <li>{seat.name}*---*{seat.guid}</li>
+      return(
+        <List.Item>
+          <List.Icon name='arrow circle right' size ='meidum' verticalAlign='middle' />
+          <List.Content>
+            <List.Header as='a'>{seat.name}</List.Header>
+            <List.Description as='a'>{seat.guid}</List.Description>
+          </List.Content>
+        </List.Item>
+      );
     });
     return (
       <React.Fragment key={1}>
         Greeting: {this.props.greeting}
         <br />
         <button className='getSeatsBtn' onClick={ () => this.props.getSeats()}>getSeats</button>
-        <br />
-        <ul> {seatsList} </ul>
-
+        <List divided relaxed>{seatsList}</List>
       </React.Fragment>
     );
   }
--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 const initialState = {
     seats: [
@@ -23,7 +25,9 @@ export default function configureStore(){
     const store = createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        composeWithDevTools(
+            applyMiddleware(thunk)
+        )
     );
     return store;
 }
