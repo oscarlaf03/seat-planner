ed3070f803ce809342b4b5ced41c7193c1bd498d
oscar ortiz <ortizg.oscar@gmail.com>
2020-08-03 22:18:05 -0300

    minor changes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import PropTypes from "prop-types"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "../configureStore";
